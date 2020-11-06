1d7c183802f3364a13ccae32d16223b8b63c357d
timmcgilly <timmcgilly@gmail.com>
2020-11-06 10:31:02 +0000

    Added basic json parsing

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/upload_data', (req, res) => {
-  res.send('Hello World!')
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.post('/api/upload_data', (req, res) => {
+  //Token verification
+  var token = req.body.id;
+
+  var cpm = req.body.reading.cpm; 
+  var floor = req.body.reading.location.floor;
+  var locX = req.body.reading.location.x; 
+  var locY = req.body.reading.location.y;
+
+  res.send("Data submitted sucessfully. cpm: "+cpm);
 })
 
 app.listen(port, () => {
