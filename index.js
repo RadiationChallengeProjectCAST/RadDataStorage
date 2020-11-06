a3d7d9c2130ce9a7539d413d4a64a8d7e103f6ae
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2020-11-06 11:16:02 +0000

    added authentication

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const client = new Pool({
     host: "localhost",
     database: "radiationDb",
     password: token.password,
-    port: "5432"    
-  });
+    port: "5432"
+});
 
 client.connect();
 
@@ -24,12 +24,23 @@ app.post('/api/upload_data', (req, res) => {
     //Token verification
     var token = req.body.id;
 
+    const result = await client.query({
+        text: 'SELECT TeamID FROM Team WHERE TeamToken = $1',
+        values: [token],
+    })
+
+    if (result.rowCount == 0) {
+        res.send("Invalid token.")
+        return;
+    }
+    var teamID = result.rows[0].TeamID
+
     var cpm = req.body.reading.cpm;
     var floor = req.body.reading.location.floor;
     var locX = req.body.reading.location.x;
     var locY = req.body.reading.location.y;
 
-    res.send("Data submitted sucessfully. cpm: " + cpm + " floor: " + floor + " locX:" + locX + " locY:" + locY);
+    res.send("Data submitted sucessfully. cpm: " + cpm + " floor: " + floor + " locX: " + locX + " locY: " + locY + "teamID: " + teamID);
 })
 
 app.listen(port, () => {
