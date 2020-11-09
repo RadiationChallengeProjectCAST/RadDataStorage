2a274993d67c131f162d122354010de1c18ccb5d
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2020-11-09 23:04:41 +0000

    disabled no-console rule for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,15 @@ module.exports = {
         ecmaVersion: 12,
     },
     rules: {
-        indent: ['error', 4],
+        'indent': ['error', 4],
+        'quote-props': ['error', 'consistent'],
+        'no-console': 'off',
+        'no-restricted-syntax': [
+            'error',
+            {
+                'selector': 'CallExpression[callee.object.name="console"][callee.property.name!=/^(log|warn|error|info|trace)$/]',
+                'message': 'Unexpected property on console object was called',
+            },
+        ],
     },
 };
